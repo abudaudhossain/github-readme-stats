30f4c35d6fdc07acb8a90692eb3a3ef51de4c7f0
Abu Daud Hossain <73416372+abudaudhossain@users.noreply.github.com>
2021-08-22 14:04:11 +0600

    Update index.js

--- themes/index.js
+++ themes/index.js
@@ -3,7 +3,7 @@ const themes = {
     title_color: "2f80ed",
     icon_color: "4c71f2",
     text_color: "333",
-    bg_color: "fffefe",
+    bg_color: "transparent",
     border_color: "e4e2e2",
   },
   default_repocard: {
